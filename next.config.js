c0bdb0653d03fb4e3ffb335ad5502c2640998bed
Pulindi Dhanesha <pulindidhanesha15@gmail.com>
2024-07-18 11:32:58 +0530

    updated basepath

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV = 'production'
+
 const nextConfig = {
+  basePath : isProd ? '/pulindi_portfolio' : '',
   output: 'export',
   reactStrictMode: true,
   swcMinify: true,
